30297f5801ae14f2c42ee11b598a96ebb14ab2aa
Ben-G <Benjamin.Encz@gmail.com>
2015-09-09 09:20:03 -0700

    [Restructure] Group Network Related Code

---
+++ TripPlanner/Networking/Synchronization/TripPlannerBackendSynchronizer.swift
@@ -0,0 +1,21 @@
+//
+//  TripPlannerBackendSynchronization.swift
+//  TripPlanner
+//
+//  Created by Benjamin Encz on 9/9/15.
+//  Copyright © 2015 Make School. All rights reserved.
+//
+
+import Foundation
+
+struct TripPlannerBackendSynchronizer {
+    
+    func downloadSync() -> Void {
+        
+    }
+    
+    func uploadSync() -> Void {
+        
+    }
+    
+}
