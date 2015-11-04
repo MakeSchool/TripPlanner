d842908029a58078448cc3851013677a0bc00999
Ben-G <Benjamin.Encz@gmail.com>
2015-11-04 15:25:50 -0800

    [JSONParsing] Reduce Redundancy Slightly

--- TripPlanner/Model/GooglePlaces/JSONParsing.swift
+++ TripPlanner/Model/GooglePlaces/JSONParsing.swift
@@ -13,38 +13,32 @@ func parse<T: Decodable where T == T.DecodedType>(data: NSData) -> T? {
   let json: AnyObject? = try! NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0))
   if let j: AnyObject = json {
     let resultDecoded: Decoded<T> = decode(j)
-    
-    switch resultDecoded {
-    case let .Success(value): return value
-    case let .TypeMismatch(error):
-      assertionFailure(error)
-      return nil
-    case let .MissingKey(error):
-      assertionFailure(error)
-      return nil
-    }
+    return evaluateDecodedResult(resultDecoded)
   } else {
     return nil
   }
 }
 
-// TODO: avoid duplication
+//// TODO: avoid duplication once constrained extensions can conform to a protocol
 
 func parse<T: Decodable where T == T.DecodedType>(data: NSData) -> [T]? {
   let json: AnyObject? = try! NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0))
   if let j: AnyObject = json {
     let resultDecoded: Decoded<[T]> = decode(j)
-    
-    switch resultDecoded {
-    case let .Success(value): return value
-    case let .TypeMismatch(error):
-      assertionFailure(error)
-      return nil
-    case let .MissingKey(error):
-      assertionFailure(error)
-      return nil
-    }
+    return evaluateDecodedResult(resultDecoded)
   } else {
+    return nil
+  }
+}
+
+func evaluateDecodedResult<T>(decoded: Decoded<T>) -> T? {
+  switch decoded {
+  case let .Success(value): return value
+  case let .TypeMismatch(error):
+    assertionFailure(error)
+    return nil
+  case let .MissingKey(error):
+    assertionFailure(error)
     return nil
   }
 }
